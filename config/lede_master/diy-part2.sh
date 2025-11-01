bca2eabfd448ca4ebc6927d8da00ec8e82263d84
M <28711483+darkswords@users.noreply.github.com>
2025-11-01 21:55:44 +0800

    Update diy-part2.sh

--- config/lede_master/diy-part2.sh
+++ config/lede_master/diy-part2.sh
@@ -21,7 +21,7 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCEBRANCH='master'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.6/g' package/base-files/files/bin/config_generate
 
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
